f4c55b0fd80fcb08f226cfbbe6a59dc8fda642a5
donghyun_daniel <donghyun.main@gmail.com>
2020-08-17 21:17:56 +0900

    2979 updated

--- BOJ/2979.py
+++ BOJ/2979.py
@@ -1,5 +1,12 @@
-A,B,C=map(int,input().split())
-L=[]
-for _ in range(3):
-    L.append(list(map(int,input().split())))
-L=sorted(L)
+C=list(map(int,input().split()))
+C[1]*=2; C[2]*=3
+car=[0 for i in range(100)]
+for i in range(3):
+    S,E=map(int,input().split())
+    for j in range(S,E):
+        car[j-1]+=1
+cost=0
+for i in range(99):
+    if car[i]!=0:
+        cost+=C[car[i]-1]
+print(cost)
