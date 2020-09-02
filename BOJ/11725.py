38b2d6b8926667b88d8a5917dbba048bc31b8ff5
donghyun_daniel <donghyun.main@gmail.com>
2020-09-02 19:01:38 +0900

    11725 updated

--- BOJ/11725.py
+++ BOJ/11725.py
@@ -1,5 +1,24 @@
+from collections import deque
+
 N=int(input())
-dic={n+1 : 0 for n in range(N)}
+dic = {i+1 : [] for i in range(N)}
+visit = [False for i in range(N+1)]
+parent = {i+2 : False for i in range(N-1)}
 for i in range(N-1):
     A,B=map(int,input().split())
-    print(dic)
+    dic[A].append(B)
+    dic[B].append(A)
+print(dic)
+
+def bfs(v):
+    q = deque([v])
+    visit[v] = False
+    while q:
+        v = q.popleft()
+        for i in dic[v]:
+            if not visit[i]:
+                visit[i]=True
+                q.append(i)
+
+bfs(1)
+
