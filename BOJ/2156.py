4bb6352cefb6f3a101631e9b0e932b2ded23a7d1
donghyun_daniel <donghyun.main@gmail.com>
2020-09-09 19:17:01 +0900

    1406, 2156 updated

--- BOJ/2156.py
+++ BOJ/2156.py
@@ -1,6 +1,15 @@
-cup=[]
-for _ in range(int(input())):
-    cup.append(int(input()))
-dp=[0 for i in range(len(cup))]
-for i in range(len(dp)):
-    dp[i]=max(cup[i])
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+wine = [0]
+for i in range(n):
+    wine.append(int(input()))
+
+dp = [[0, 0, 0] for i in range(n + 1)]
+dp[1] = [0, wine[1], 0]
+for i in range(2, n + 1):
+    dp[i][0] = max(dp[i - 1])
+    dp[i][1] = dp[i - 1][0] + wine[i]
+    dp[i][2] = dp[i - 1][1] + wine[i]
+print(max(dp[n]))
