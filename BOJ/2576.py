5e578b665f6c6dc43e78d51df9fc870947fc7bc6
donghyun-daniel <donghyun.main@gmail.com>
2020-12-15 20:32:03 +0900

    2576 updated

--- BOJ/2576.py
+++ BOJ/2576.py
@@ -1,8 +1,5 @@
-num=[]
-for i in range(7):
-    n=int(input())
-    if n%2==1:
-        num.append(n)
+num=[int(input()) for i in range(7)]
+num = list(filter(lambda x : x%2 == 1, num))
 if num:
     print(sum(num))
     print(min(num))
